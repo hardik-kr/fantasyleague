2aa83939880ee01bf717767d0f3319db9b0c208b
hardik-kr <hardikpraj2009@gmail.com>
2026-04-26 01:14:23 +0530

    master data no auth

--- src/main/java/com/cricket/fantasyleague/security/SecurityConfig.java
+++ src/main/java/com/cricket/fantasyleague/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig
     {
         http.csrf(csrf->csrf.disable())
             .cors(cors->cors.disable())
-            .authorizeHttpRequests(auth->auth.requestMatchers("/auth/login","/auth/signup/**","/api/loadtest/**","/api/masterdata/**","/test/**","/health","/health/**","/actuator/**").permitAll()
+            .authorizeHttpRequests(auth->auth.requestMatchers("/auth/login","/auth/signup/**","/api/loadtest/**","/api/masterdata/**","/api/data/**","/test/**","/health","/health/**","/actuator/**").permitAll()
             .requestMatchers("/season/**").hasAnyAuthority(UserRole.USER.name())
             .anyRequest().authenticated())
             .exceptionHandling(ex->ex.authenticationEntryPoint(point)
